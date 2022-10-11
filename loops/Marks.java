766e061c710e8221b95909902f665fa6d690fd9e
Darshu1221 <111422864+Darshu1221@users.noreply.github.com>
2022-10-10 22:47:11 -0700

    Add files via upload

---
+++ loops/Marks.java
@@ -0,0 +1,40 @@
+package loops;
+	
+//Java Program to Enter Marks of Five Subjects and Calculate Total, Percentage, and Grade
+
+import java.util.Scanner;
+ class Marks {
+	
+
+	 public static void main(String[] args) {
+		   Scanner sc = new  Scanner(System.in);
+		
+		   { 
+			   System.out.println("Enter Your name:-"); 
+			   String n1 =sc.next();
+		   }
+	     
+
+	     System.out.println("Enter the marks of five subjects:-\n");
+
+	     // To store the values of five subjects
+	     float sub_1 = sc.nextFloat();
+	     float sub_2 = sc.nextFloat();
+	     float sub_3 = sc.nextFloat();
+	     float sub_4 = sc.nextFloat();
+	     float sub_5 = sc.nextFloat();
+
+	     float total;
+		   sc.close();
+
+	     // Calculate total, average and percentage
+	     total = sub_1 + sub_2 + sub_3 + sub_4 + sub_5;
+
+	     
+
+	     // It will produce the final output
+	     System.out.println("\nThe Total marks   = " + total + "/500.0");
+	   
+	}
+	}
+
---
+++ loops/Voting.java
@@ -0,0 +1,28 @@
+package loops;
+//Java program to check vote eligibility 
+import java.util.Scanner;
+
+ class Voting {
+public static void main(String[] args) 
+{
+ // Declaring variables
+ int age, diff;
+ // taking user input
+ Scanner sc = new  Scanner(System.in); 
+ System.out.println("Please enter your age: ");
+ age = sc.nextInt();
+ 
+ sc.close();
+
+ // Checking condition for voting..
+ if(age>=18)
+ {
+     System.out.println("You are eligible for voting.");
+ }
+ else
+ {
+ diff = (18 - age);
+ System.out.println("Sorry,You can vote after: "+ diff + " years");
+ }
+}
+}
