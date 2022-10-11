67464bbd1e9cc3c6b68c630efddd4da384de79a2
Darshu1221 <111422864+Darshu1221@users.noreply.github.com>
2022-10-10 23:45:07 -0700

    Add files via upload

---
+++ loops/WeekDay.java
@@ -0,0 +1,60 @@
+//**Program to demonstrate(Switch) asnd printing week days
+
+
+
+package Swich;
+import java.util.Scanner;
+
+
+
+public class WeekDay {
+	public static void main(String[] args) {
+		// take input form the User.
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ente the Number(1-7) :");
+
+		int ND = sc.nextInt();
+		switch (ND) {
+		case 1:
+
+			System.out.println("This is Monday");
+			break;
+
+		case 2:
+
+			System.out.println("This is Tuesday");
+			break;
+
+		case 3:
+
+			System.out.println("This is Wednesday");
+			break;
+
+		case 4:
+
+			System.out.println("This is Thursday");
+			break;
+
+		case 5:
+
+			System.out.println("This is Firday");
+			break;
+
+		case 6:
+
+			System.out.println("This is Saturday");
+			break;
+
+		case 7:
+
+			System.out.println("This is Sunday");
+			break;
+
+		default:
+			System.out.println(ND + " Day does not Present!!");
+			break;
+
+		}
+		sc.close();
+	}
+}
